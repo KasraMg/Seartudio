d91acb8c262ef40f938f2f19f079ee57e31dff8f
KasraMg <kasragithub7@gmail.com>
2023-04-21 22:28:58 +0430

    404

---
+++ src/pages/404/404.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './404.css'
+import CustomHeader from '../../components/CustomHeader/CustomHeader'
+export default function NotFound() {
+  return (
+    <div className='Not-Found'>
+        <CustomHeader/>
+        <main>
+            <img src="../../public/images/similar/Group 24.png" alt="404" />
+            <p className='mt-4'>گشتم <span>نبود</span>!</p>
+            <p><span>نگرد</span> که نیست ):</p>
+        </main>
+    </div>
+  )
+}
--- src/router.jsx
+++ src/router.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Index from "./pages/Index/Index";
 import Rules from "./pages/Rules/Rules";
 import AboutUs from "./pages/AboutUs/AboutUs";
+import NotFound from "./pages/404/404";
 const routes = [
   { path: "/", element: <Index /> },
   { path: "/Rules", element: <Rules /> },
   { path: "/AboutUs", element: <AboutUs /> },
+  { path: "/*", element: <NotFound /> },
   
 ];
 
